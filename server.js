88105b22c001c99f3fa44e02e0ecb3194be23c30
theJoseNeto <joseneto180201@gmail.com>
2022-01-21 16:58:26 -0300

    .

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = require('express')()
 const {json, urlencoded} = require('express')
 const cors = require('cors');
 
-const HOST = process.env.HOST
+const PORT = process.env.PORT || 3333;
 const routes = require("./src/routes/index.routes");
 
 app.use(cors());
 app.use(json());
 app.use(urlencoded({extended: true}));
 app.use(routes);
-app.listen(HOST, ()=> console.log('App is running.'));
+app.listen(PORT, ()=> console.log('App is running.'));
